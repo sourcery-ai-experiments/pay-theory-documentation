cc96c519aaa39806f0d17943ed918a719b3aff55
Blake A Galloway <36147439+blakeAgalloway@users.noreply.github.com>
2023-07-03 10:59:56 -0400

    major css styling changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,6 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
+//const lightCodeTheme = require('./src/css/prismLight.js');
+
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
